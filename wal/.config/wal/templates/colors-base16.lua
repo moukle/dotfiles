b19c066ca8741aa54d22951def31cd588b3df79c
Klein, Moritz <4622472+moukle@users.noreply.github.com>
2021-09-01 10:59:45 +0200

    Update before re-install

---
+++ wal/.config/wal/templates/colors-base16.lua
@@ -0,0 +1,18 @@
+return {{
+   base00 = "{background.strip}",
+   base01 = "{color0.strip}",
+   base02 = "{color8.strip}",
+   base03 = "{color8.strip}",
+   base04 = "{color7.strip}",
+   base05 = "{foreground.strip}",
+   base06 = "{color15.strip}",
+   base07 = "{color8.strip}",
+   base08 = "{color1.strip}",
+   base09 = "{color11.strip}",
+   base0A = "{color3.strip}",
+   base0B = "{color2.strip}",
+   base0C = "{color6.strip}",
+   base0D = "{color4.strip}",
+   base0E = "{color5.strip}",
+   base0F = "{color9.strip}",
+}}
