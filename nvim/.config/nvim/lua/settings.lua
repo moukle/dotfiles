34a273d9277e1cc7e8f64401125295aab6928c58
Moritz Klein <moritz.klein@pm.me>
2021-12-14 22:31:03 +0100

    Add nvim comment and some general settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -2,6 +2,17 @@ local vim = vim
 local cmd = vim.cmd
 local opt = vim.opt
 
+-- Temporary files {{{
+opt.undofile = true
+opt.undodir = vim.fn.stdpath('config') .. '/undo'
+
+opt.backup = true
+opt.backupcopy = 'auto'
+opt.backupdir = vim.fn.stdpath('config') .. '/backup'
+
+opt.swapfile = true
+opt.directory = vim.fn.stdpath('config') .. '/swap'
+-- }}}
 -- UI {{{
 opt.termguicolors = true
 cmd [[colorscheme term]]
@@ -9,6 +20,30 @@ cmd [[colorscheme term]]
 opt.number = true
 opt.relativenumber = true
 
+-- Show matching brackets when text indicator is over them
+opt.showmatch = true
+-- }}}
+-- Folding {{{
+opt.foldmethod = 'expr'
+opt.foldexpr = 'nvim_treesitter#foldexpr()'
 opt.fillchars = {eob = " " , vert = "┃", fold = " " }
 opt.foldtext = [[v:lua.FoldText()]]
 -- }}}
+-- Search {{{
+opt.ignorecase = true
+opt.smartcase = true
+-- }}}
+-- Indentation {{{
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.smartindent = true
+
+opt.list = true
+opt.listchars = 'tab:│ ,trail:━'
+
+opt.wrap = true
+--}}}
+
+-- vim: foldmethod=marker foldlevel=0 foldenable foldmarker={{{,}}}
