fd2e6f96f369ae6cfa5fc0f0c422206914ec323c
Moritz Klein <moritz.klein@pm.me>
2022-08-03 13:21:21 +0200

    nvim: use global statusline

--- nvim/.config/nvim/lua/config/lualine-nvim.lua
+++ nvim/.config/nvim/lua/config/lualine-nvim.lua
@@ -2,14 +2,16 @@ local lualine = require 'lualine'
 
 lualine.setup {
 	options = {
+        globalstatus = true,
 		icons_enabled = true,
 		component_separators = '',
 		section_separators = { left = '', right = '' },
 	},
 	sections = {
 		lualine_a = { {'mode', separator = { left = ' ', right = '' }, fmt = function(str) return str:sub(1,1) end}, },
-		lualine_b = { {'filename', file_status = true, separator = {left = ' '} } },
-		lualine_c = { {'branch', } },
+		-- lualine_b = { {'filename', file_status = true, separator = {left = ' '} } },
+		lualine_b = { {'branch', } },
+        lualine_c = {},
 		lualine_x = {},
 		--lualine_y = { 'filetype', 'progress' },
 		lualine_y = {},
@@ -24,14 +26,23 @@ lualine.setup {
 		lualine_y = { { 'location', separator = { left = ' ', right = ' ' }, left_padding = 2 }, },
 	},
     tabline = {
-        lualine_a = { 'buffers' },
+        lualine_a = {
+            {
+                'buffers',
+                buffers_color = {
+                    -- Same values as the general color option can be used here.
+                    active = 'DiagnosticWarning',     -- Color for active buffer.
+                    -- inactive = 'ErrorMsg', -- Color for inactive buffer.
+                }
+            }
+        },
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {'tabs'}
     },
-	tabline = {},
+	-- tabline = {},
 	extensions = {},
 }
 
